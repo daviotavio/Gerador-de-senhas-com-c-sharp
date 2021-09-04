0435225285a9b83642acdae4ebb15c641ab6d551
Davi Otavio <85647007+daviotavio@users.noreply.github.com>
2021-09-04 20:18:08 -0300

    Add files via upload

---
+++ atividade2-Davi Otávio/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class MyProgram2
+{
+    public static void Main(string[] args)
+    {
+        
+        string nome;
+        int idade;
+        
+        Console.Write("Digite seu nome: ");
+        nome = Console.ReadLine();
+
+        Console.Write("Digite sua idade: ");
+        idade = Convert.ToInt32(Console.ReadLine());
+       
+       if (idade >= 18 )
+        {
+        Console.WriteLine("Sugestão para senha: " +idade+nome);
+        } 
+        else 
+        {
+        Console.WriteLine("Digite sua idade: " +nome+idade);
+        }
+
+    }
+}
